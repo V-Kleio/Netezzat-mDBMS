34c763be08a14e3434f5e34143cc856f4ae4d3ab
Arlow5761 <arlow5761@gmail.com>
2025-11-13 15:40:30 +0700

    refactor: update mock FRM to conform to finalized interfaces

--- src/mDBMS.CLI/Mocks/MockFailureRecovery.cs
+++ src/mDBMS.CLI/Mocks/MockFailureRecovery.cs
@@ -1,10 +1,20 @@
-using System;
-using mDBMS.QueryProcessor.Contracts;
+using mDBMS.Common.Interfaces;
+using mDBMS.Common.Transaction;
 
 namespace mDBMS.CLI.Mocks
 {
-    public class MockFailureRecovery : IFailureRecovery
+    public class MockFailureRecovery : IFailureRecoveryManager
     {
+        public void Recover(RecoverCriteria criteria)
+        {
+            Console.WriteLine($"[MOCK FRM]: Recover dipanggil.");
+        }
+
+        public void SaveCheckpoint()
+        {
+            Console.WriteLine($"[MOCK FRM]: SaveCheckpoint dipanggil.");
+        }
+
         public void WriteLog(ExecutionResult info)
         {
             Console.WriteLine($"[MOCK FRM]: WriteLog dipanggil. Success={info.Success}, Message='{info.Message}'");
