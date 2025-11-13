834d7056989f0aee5db8745ca1e33c1bd839b870
Fozzzzy <andrewtedj@gmail.com>
2025-11-13 13:51:45 +0700

    fix: unintended changes execution result

--- src/mDBMS.Common/Transaction/ExecutionResult.cs
+++ src/mDBMS.Common/Transaction/ExecutionResult.cs
@@ -8,9 +8,9 @@ namespace mDBMS.Common.Transaction
     /// </summary>
     public class ExecutionResult
     {
-        public string Query { get; set; } = string.Empty;
+        public string Query { get; set; }
         public bool Success { get; set; }
-        public string Message { get; set; } = string.Empty;
+        public string Message { get; set; }
         public DateTime ExecutedAt { get; set; }
         public IEnumerable<Row>? Data { get; set; }
 
