e3b17bdf313f2749bc0caa97bff98c00b7f1f15b
Arlow5761 <arlow5761@gmail.com>
2025-12-06 23:10:41 +0700

    chore: add unit testing for qp

---
+++ src/mDBMS.QueryProcessorDriver/Mocks/MockFailureRecoveryManager.cs
@@ -0,0 +1,40 @@
+using mDBMS.Common.Interfaces;
+using mDBMS.Common.Transaction;
+
+namespace mDBMS.QueryProcessorDriver.Mocks;
+
+public class MockFailureRecoveryManager : IFailureRecoveryManager
+{
+    private readonly TestObserver _observer;
+
+    public MockFailureRecoveryManager(TestObserver observer)
+    {
+        _observer = observer;
+    }
+
+    public void WriteLog(ExecutionResult result)
+    {
+        _observer.Record($"FailureRecoveryManager.WriteLog(Query='{result.Query}', Success={result.Success})");
+    }
+
+    public void SaveCheckpoint()
+    {
+        _observer.Record("FailureRecoveryManager.SaveCheckpoint");
+    }
+
+    public bool UndoTransaction(int transactionId)
+    {
+        _observer.Record($"FailureRecoveryManager.UndoTransaction(ID={transactionId})");
+        return true;
+    }
+
+    public void WriteLog(mDBMS.Common.Transaction.ExecutionLog log)
+    {
+        _observer.Record($"FailureRecoveryManager.WriteLog");
+    }
+
+    public void Recover(RecoverCriteria criteria)
+    {
+        _observer.Record("FailureRecoveryManager.Recover");
+    }
+}
